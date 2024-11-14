5cc7cf69da34d966c44a6f4e7c012821bcaaf7e9
tufusa <tufusa74@gmail.com>
2024-11-14 15:23:37 +0900

    feat: GuildControllerを実装

---
+++ src/controller/guild.ts
@@ -0,0 +1,28 @@
+import type { Result } from "@mikuroxina/mini-fn";
+import type {
+  APIGuildMember,
+  RESTError,
+  RESTPatchAPICurrentGuildMemberJSONBody,
+} from "discord-api-types/v10";
+import type { FetcherService } from "../service/fetcher";
+import { ModifyGuildMemberService } from "../service/guild/modifyMember";
+
+export class GuildController {
+  private readonly modifyMember: ModifyGuildMemberService;
+
+  constructor(fetcher: FetcherService) {
+    this.modifyMember = new ModifyGuildMemberService(fetcher);
+  }
+
+  /**
+   * Modify the current member in a guild.
+   * @param guildId Guild ID to modify member
+   * @param body Member
+   */
+  async modifyCurrentMember(
+    guildId: string,
+    body: RESTPatchAPICurrentGuildMemberJSONBody
+  ): Promise<Result.Result<RESTError | Error, APIGuildMember>> {
+    return this.modifyMember.modifyCurrentMember(guildId, body);
+  }
+}
